0f2a840cdc84c3c5e178e8b796916f35954809be
Jhansi <jhansi2728@gmail.com>
2025-06-29 11:46:45 +0530

    Added script to fetch currency exchange rates

---
+++ currency_etl.py
@@ -0,0 +1,12 @@
+import requests
+
+def fetch_exchange_rates():
+    url = "https://api.exchangerate-api.com/v4/latest/USD"
+    response = requests.get(url)
+    data = response.json()
+    
+    print("Base Currency:", data["base"])
+    print("Rates:", data["rates"])
+
+if __name__ == "__main__":
+    fetch_exchange_rates()
